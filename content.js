b966c0145decfcdfcefc073a9a7fdf853704d2a4
qiuyiliang <pengwenlong245@gmail.com>
2022-12-20 14:09:03 +0800

    feat(fixed)

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ const tabs = document.createElement("iframe");
 tabs.setAttribute("src", chrome.extension.getURL("tabs.html"));
 tabs.style.position = "fixed";
 tabs.style.height = "40px";
-tabs.style.top = "-40px";
+tabs.style.top = "0";
 tabs.style.left = "0";
 tabs.style.width = "100vw";
 tabs.style.zIndex = "9999999";
